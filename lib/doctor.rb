1f535c52283ad8dbe7929509c0ef4350b79a9966
Sarah Elzinga <sevans924@gmail.com>
2019-04-08 09:01:03 -0500

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,34 @@
+class Doctor
+
+attr_reader :name
+
+@@all = []
+
+def initialize(name)
+  @name = name
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
+def new_appointment(patient, date)
+  Appointment.new(date, patient, self)
+end
+
+def appointments
+  Appointment.all.select do |appt|
+    appt.doctor == self
+  end
+end
+
+  def patients
+    # Appointment.all.map do |appt|
+    #   appt.patient
+    # end
+    self.appointments.map {|app| app.patient}
+  end
+
+
+end
